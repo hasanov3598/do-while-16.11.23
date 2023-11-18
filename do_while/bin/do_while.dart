cb974c63393880bc00abe1a356b44d9a94936ac1
hasanov3598 <bahramov3598@gmail.com>
2023-11-18 20:19:10 +0600

    first commit

---
+++ do_while/bin/do_while.dart
@@ -0,0 +1,148 @@
+//void main() {
+  //1
+  // List a = ["яблоко", "груша", "апельсин", 'конец', "банан"];
+  // int i = 0;
+  // while (i < a.length) {
+  //   print(a[i]);
+  //   i++;
+  //   if (a[i] == "конец") {
+  //     break;
+  //   }
+  // }
+  //2
+
+  //3
+  // List<int> a = [2, 5, 8, 10, 15];
+  // int i = 0;
+  // int summ = 0;
+  // do {
+  //   if (a[i].isEven) {
+  //     summ += a[i];
+  //   }
+  //   i++;
+  // } while (i < a.length);
+  // print(summ);
+
+  //4
+  // List a = [2.5, 4.8, 7.2, 1.5, 9.6];
+  // double summ = 0;
+  // for (var i in a) {
+  //   summ += i;
+  // }
+  // print(summ / a.length);
+
+  //5
+  // List a = [8, 12, 15, 20, 5];
+  // int i = 0;
+  // while (i < a.length) {
+  //   if (a[i] > 10) {
+  //     print(a[i] * 2);
+  //   }
+  //   i++;
+  // }
+
+  //6
+  // List a = [3, 7, 4, 10, 2];
+  // int i = 0;
+  // do {
+  //   if (a[i] > 5) {
+  //     print(a[i]);
+  //   }
+  //   i++;
+  // } while (i < a.length);
+
+  //7
+  // List a = ["Апельсин", "Банан", "Арбуз", "Груша", "Мандарин"];
+  // int i = 0;
+  // while (i < a.length) {
+  //   if (a[i][0] == "А") {
+  //     print(a[i]);
+  //   }
+  //   i++;
+  // }
+
+  //8
+  // List a = [4, -7, 10, -3, 8];
+  // int i = 0;
+  // do {
+  //   if (a[i] > 0) {
+  //     print(a[i] * 2);
+  //   } else {
+  //     print(a[i] / 2);
+  //   }
+  //   i++;
+  // } while (i < a.length);
+
+  //9
+  // List a = [-3, 7, -1, 9, -5];
+  // int i = 0;
+  // int summ = 0;
+  // while (i < a.length) {
+  //   if (a[i] > 0) {
+  //     summ += a[i] as int;
+  //   }
+  //   i++;
+  // }
+  // print(summ);
+
+  //10
+  // List a = [5, -3, 8, -2, 10];
+  // for (int i = 0; i < a.length; i++) {
+  //   if (a[i] < 0) {
+  //     a.replaceRange(
+  //       i,
+  //       i + 1,
+  //       [
+  //         a[i] * a[i],
+  //       ],
+  //     );
+  //   }
+  // }
+  // print(a);
+
+  //11
+
+  // List a = [2, 6, 8, -1, 3];
+
+  // int i = 0;
+  // do {
+  //   if (a[i] > 0) {
+  //     print(a[i]);
+  //   } else {
+  //     break;
+  //   }
+  //   i++;
+  // } while (i < a.length);
+
+  //12
+
+  // List a = [1.5, 2.0, 3.5, 4.0, 2.5];
+  // for (int i = 0; i < a.length; i++) {
+  //   print(a[i] * i);
+  // }
+
+  //13
+  // List a = [3, 6, 9, 2, 5];
+  // int i = 0;
+  // int summ = 0;
+  // while (i < a.length) {
+  //   if (a[i] % 3 == 0) {
+  //     summ += a[i] as int;
+  //   }
+  //   i++;
+  // }
+  // print(summ);
+
+  //14
+  // List a = [3, 0, -5, 8, -2];
+  // for (var i in a) {
+  //   if (i > 0) {
+  //     print("$i + ");
+  //   } else if (i == 0) {
+  //     print("$i - 0");
+  //   } else {
+  //     print("$i -");
+  //   }
+  // }
+
+//}
